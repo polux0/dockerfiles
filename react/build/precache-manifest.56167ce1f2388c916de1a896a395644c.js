9079b5d3c0cee5185c05176483fd4857d27d630c
alexus <afidelo@protonmail.com>
2020-04-26 01:16:10 +0200

    react `dev` and and production ready (nginx) dockerfiles

---
+++ react/build/precache-manifest.56167ce1f2388c916de1a896a395644c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df2fab88fdefc4fe54b9dbe050ad88c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b78edb1bb6b0df91fdfa",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e93d441e584b5f1dacff",
+    "url": "/static/js/2.53b6bb5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53b6bb5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78edb1bb6b0df91fdfa",
+    "url": "/static/js/main.51519d0f.chunk.js"
+  },
+  {
+    "revision": "c71849e6b92a54d9b65d",
+    "url": "/static/js/runtime-main.dbac8e30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
